954b032abdb5d26ee347c3f9d5250491a2863816
Ruchira89392002 <125777480+Ruchira89392002@users.noreply.github.com>
2023-10-06 09:13:04 +0530

    Add files via upload

---
+++ plugins/main-Guru.js
@@ -0,0 +1,33 @@
+let handler = async m => m.reply(`
+
+≡  *DS  ┃ ᴮᴼᵀ*   GROUPS
+
+─────────────
+▢ Join public bot group and support
+*Comming sooon*
+
+▢ Group 2
+*Comming sooon*
+
+▢ Group 3
+*Comming sooon*
+
+─────────────
+≡ Disabled links? enter here! 
+
+▢ Group WhatsApp 
+ *Comming sooon*
+─────────────
+▢ *Owner Telegram*
+ *NOT use telegaram*
+
+▢ *YouTube*
+• *Comming sooon*
+
+
+`.trim())
+handler.help = ['gpguru']
+handler.tags = ['main']
+handler.command = ['groups', 'groupguru', 'gugp', 'ggp', 'gpguru'] 
+
+export default handler
