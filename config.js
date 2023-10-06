424753c212e0f0a263bd22a6a0151ed138f0593d
Ruchira89392002 <125777480+Ruchira89392002@users.noreply.github.com>
2023-10-06 10:01:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -50,7 +50,7 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = 'DARK SHADOW V2'
 global.premium = 'true'
 global.packname = 'GURU┃ᴮᴼᵀ' 
 global.author = '@Asliguru' 
